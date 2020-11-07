8d86aae8e0e617935af5fbc9b18f54369e30bf60
Figurova Olga <yespechenko@gmail.com>
2020-11-07 15:57:09 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,17 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    //задаем ожидаемое значение
+    long expected = 30;
+
+    //вызываем целевой метод
+    long actual = service.calculate(amount, registered);
+
+    //производим проверку (сравниваем ожидаемый и фактический):
+    boolean passed = expected == actual;
+
+    // выводим результат
+    System.out.println(passed);
   }
 }
 
